88da468e74a525d6c2dea215117d8a06a267d155
huhui <569263082@qq.com>
2019-09-26 16:26:33 +0800

    增加token验证

--- vue-blog-client/src/views/Home.vue
+++ vue-blog-client/src/views/Home.vue
@@ -18,7 +18,6 @@ export default {
     this.img();
   },
   mounted() {
-    this.fetchData();
     this.changText()
   },
 computed:{
@@ -51,13 +50,6 @@ computed:{
       ]
       let i = Math.round(Math.random() * 10);
       this.text = this.textArray[i]
-    },
-
-    fetchData: async function() {
-      const res = await this._api.test();
-      if (res.data.success) {
-        alert("请求成功");
-      }
     }
   }
 };
