5019d253846697f7b652c2e1c63dba9eb932911d
huhui <569263082@qq.com>
2019-10-14 11:28:31 +0800

    进行webpack优化处理

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const productionGzipExtensions = ['js', 'css'];
+const isProduction = process.env.NODE_ENV === 'production';
 function resolve(dir) {
   return path.join(__dirname, '.', dir);
 }
@@ -19,5 +22,44 @@ module.exports = {
     .options({
       symbolId: 'icon-[name]'
     });
+    if (isProduction) {
+      // 开启gzip压缩
+      config.plugins.push(new CompressionWebpackPlugin({
+       algorithm: 'gzip',
+       test: /\.js$|\.html$|\.json$|\.css/,
+       threshold: 10240,
+       minRatio: 0.8
+      }))
+     }
+     if (isProduction) {
+      // 开启分离js,打出来的包chunk.js太大了。
+      config.optimization = {
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 20000,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name (module) {
+                // get the name. E.g. node_modules/packageName/not/this/part.js
+                // or node_modules/packageName
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                return `npm.${packageName.replace('@', '')}`
+              }
+            }
+          }
+        }
+      };
+    }
+    if (isProduction) {  //开启静态资源,cdn加载。
+      config.externals = {
+       'vue': 'Vue',
+       'vue-router': 'VueRouter',
+       'moment': 'moment'
+      }
+     }
   }
 }
